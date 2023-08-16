0091b5ef899a412f7f23aaab167964b38433920a
user <cjlee6009@gmail.com>
2023-08-16 12:18:21 +0900

    다이어 그램 설계

---
+++ src/main/java/OrderService/ServiceRepository.java
@@ -0,0 +1,12 @@
+package OrderService;
+
+public interface ServiceRepository {
+
+    void Order();
+
+    void Order_check();
+
+    void Order_del();
+
+    void reOrder();
+}
---
+++ src/main/java/OrderService/ServiceRepositoryImpl.java
@@ -0,0 +1,23 @@
+package OrderService;
+
+public class ServiceRepositoryImpl implements ServiceRepository{
+    @Override
+    public void Order() {
+
+    }
+
+    @Override
+    public void Order_check() {
+
+    }
+
+    @Override
+    public void Order_del() {
+
+    }
+
+    @Override
+    public void reOrder() {
+
+    }
+}
