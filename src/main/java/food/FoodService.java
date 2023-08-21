305a13458d9aaa1a4bdebf1d016df65986e4e680
user <cjlee6009@gmail.com>
2023-08-21 12:42:30 +0900

    FoodService 의존성 주입

--- src/main/java/food/FoodService.java
+++ src/main/java/food/FoodService.java
@@ -2,15 +2,26 @@ package food;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 public class FoodService {
 
-    @Autowired
-    private FoodRepository foodRepository;
-    public Long join(Food food){
+    private final FoodRepository foodRepository;
 
+    @Autowired
+    public FoodService(FoodRepository foodRepository) {
+        this.foodRepository = foodRepository;
+    }
+
+    @Transactional
+    public Long join(Food food){
         foodRepository.save(food);
         return food.getId();
+    }
+
+    public Food findOne(Long foodId){
+        return foodRepository.findOne(foodId);
     }
 }
